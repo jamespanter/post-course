e0dd821fcd7a99979a9c61b26db6c001ad2956e8
jamespanter <james.panter@blueyonder.co.uk>
2020-04-21 17:14:06 +0100

    Completed q12

--- Javascript Katas/solutions/q12-challenge.ts
+++ Javascript Katas/solutions/q12-challenge.ts
@@ -1,3 +1,9 @@
 // 12: How can a given string be reversed using recursion?
 
-export let reverseWithRecursion = (text: string): string => {};
+export let reverseWithRecursion = (text: string): string => {
+  if (text != "") {
+    return reverseWithRecursion(text.substr(1)) + text.charAt(0);
+  } else {
+    return text;
+  }
+};

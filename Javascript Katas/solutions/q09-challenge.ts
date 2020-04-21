4004c0113a60ceb015a6506c01a4f6edcb2cc9d9
jamespanter <james.panter@blueyonder.co.uk>
2020-04-21 15:36:00 +0100

    Completed q09

--- Javascript Katas/solutions/q09-challenge.ts
+++ Javascript Katas/solutions/q09-challenge.ts
@@ -1,6 +1,16 @@
 // 09: How do you check if two strings are anagrams of each other?
 
-export let areAnagramsOfEachOther = (
-  word1: string,
-  word2: string
-): boolean => {};
+export const compareAnagrams = (word1: string, word2: string): boolean => {
+  const arrayOfWord1Chars: string[] = word1.toLowerCase().split("").sort();
+  const arrayOfWord2Chars: string[] = word2.toLowerCase().split("").sort();
+
+  for (let index = 0; index < arrayOfWord1Chars.length; index++) {
+    if (
+      arrayOfWord1Chars[index] !== arrayOfWord2Chars[index] ||
+      arrayOfWord1Chars.length !== arrayOfWord2Chars.length
+    ) {
+      return false;
+    }
+  }
+  return true;
+};

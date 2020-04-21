92feb7cb819fd368eb0d7c8af5de9299d34276e8
jamespanter <james.panter@blueyonder.co.uk>
2020-04-21 16:36:25 +0100

    Completed q11

--- Javascript Katas/solutions/q11-challenge.ts
+++ Javascript Katas/solutions/q11-challenge.ts
@@ -1,3 +1,17 @@
 // 11: How do you check if a given string is a palindrome?
 
-export let palindrome = (text: string): boolean => {};
+export const palindrome = (text: string): boolean => {
+  const arrayOfChars: string[] = text
+    .toLowerCase()
+    .replace(/ /g, "")
+    .replace(/_/g, "")
+    .replace(/[^\w\s]/gi, "")
+    .split("");
+
+  for (let index = 0; index < arrayOfChars.length / 2; index++) {
+    if (arrayOfChars[index] !== arrayOfChars[arrayOfChars.length - index - 1]) {
+      return false;
+    }
+  }
+  return true;
+};

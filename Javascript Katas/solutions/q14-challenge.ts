da3a1da208bf18ba2cf20b04b45c680f4aa6d3f1
jamespanter <james.panter@blueyonder.co.uk>
2020-04-22 15:07:48 +0100

    Completed q14

--- Javascript Katas/solutions/q14-challenge.ts
+++ Javascript Katas/solutions/q14-challenge.ts
@@ -1,2 +1,11 @@
 // 14: How do you swap two numbers without using a third variable?
-export let swapWithoutThirdVariable = (varArray) => {};
+export const swapWithoutThirdVariable = (varArray) => {
+  let a: number = varArray[0];
+  let b: number = varArray[1];
+
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+
+  return [a, b];
+};

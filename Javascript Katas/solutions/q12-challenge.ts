21fd8583054504c323b3257b73812d45af0b9abe
jamespanter <james.panter@blueyonder.co.uk>
2020-04-22 13:41:24 +0100

    Completed q13

--- Javascript Katas/solutions/q12-challenge.ts
+++ Javascript Katas/solutions/q12-challenge.ts
@@ -1,6 +1,6 @@
 // 12: How can a given string be reversed using recursion?
 
-export let reverseWithRecursion = (text: string): string => {
+export const reverseWithRecursion = (text: string): string => {
   if (text != "") {
     return reverseWithRecursion(text.substr(1)) + text.charAt(0);
   } else {
--- Javascript Katas/solutions/q13-challenge.ts
+++ Javascript Katas/solutions/q13-challenge.ts
@@ -1,3 +1,16 @@
 // 13. How do you check if two strings are a rotation of each other?
 
-export let areRotations = (text1: string, text2: string): boolean => {};
+export const areRotations = (text1: string, text2: string): boolean => {
+  if (text1.length === text2.length) {
+    for (let index = 0; index < text1.length; index++) {
+      let isRotation: string = `${text1.substr(index)}${text1.substr(
+        0,
+        index
+      )}`;
+      if (isRotation === text2) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
